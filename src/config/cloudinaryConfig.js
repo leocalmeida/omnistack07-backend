1eaf910db4eae02f694279bd27c3bae51cc4c2ee
leocalmeida <leonardocaetanoalmeida@gmail.com>
2020-05-12 08:00:08 -0300

    Integrando o clou

--- src/config/cloudinaryConfig.js
+++ src/config/cloudinaryConfig.js
@@ -1,9 +1,5 @@
 const cloudinary = require("cloudinary").v2;
 
-cloudinary.config({
-  cloud_name: "almleoc",
-  api_key: "651949951378585",
-  api_secret: "4RvD0OKF7JsmhokH_ah90VR8tY8",
-});
+cloudinary.config({});
 
 module.exports = cloudinary;
