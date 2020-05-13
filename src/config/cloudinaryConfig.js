da0efeb6fedd0b9f09896e90618852f5c1754bb6
leocalmeida <leonardocaetanoalmeida@gmail.com>
2020-05-13 12:41:26 -0300

    atualizacoes no upload da imagem

--- src/config/cloudinaryConfig.js
+++ src/config/cloudinaryConfig.js
@@ -1,5 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
-cloudinary.config({});
+cloudinary.config({
+  cloud_name: "almleoc",
+  api_key: "651949951378585",
+  api_secret: "4RvD0OKF7JsmhokH_ah90VR8tY8",
+});
 
 module.exports = cloudinary;
---
+++ src/config/mongoConnect.js
@@ -0,0 +1,10 @@
+
+const connection = 
+  "mongodb+srv://almleoc:lcaajj21@cluster0-mme2e.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+
+
+module.exports = connection;
